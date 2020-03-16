a884df7947b61be4afc11fd57d5527b0e0cd2343
alex <alextotalk@gmail.com>
2020-03-16 14:40:08 +0200

    Updates

---
+++ precache-manifest.2ed4d31e7ffe2a7224f22cb56af6a53c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561138847002aa11e2533ba44e7e4997",
+    "url": "/tasks_ignat/index.html"
+  },
+  {
+    "revision": "2b86c7d3aeb7f169f766",
+    "url": "/tasks_ignat/static/css/main.ca54527b.chunk.css"
+  },
+  {
+    "revision": "8635e8d7fb363b94faf0",
+    "url": "/tasks_ignat/static/js/2.55a5051e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tasks_ignat/static/js/2.55a5051e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b86c7d3aeb7f169f766",
+    "url": "/tasks_ignat/static/js/main.6ae510e7.chunk.js"
+  },
+  {
+    "revision": "51f9dc00c0481b0640fd",
+    "url": "/tasks_ignat/static/js/runtime-main.8927566f.js"
+  }
+]);
